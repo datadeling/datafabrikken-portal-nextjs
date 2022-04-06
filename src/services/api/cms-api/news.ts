0041d82cae9a19bd3e6c8e393d5d47f2e53e239a
Øyvind Isaksen <oyvindisaks1@gmail.com>
2022-04-06 08:24:02 +0200

    fix: include field_media_image to drupal-api response

--- src/services/api/cms-api/news.ts
+++ src/services/api/cms-api/news.ts
@@ -13,7 +13,7 @@ export const getNews = (pageLimit: number) =>
 
 export const getCmsPage = (id: string) =>
   cmsApiGet(
-    `/node/news/${id}?include=field_modules,field_modules.field_image,field_modules.field_image.field_media_image,field_global_taxonomy,field_modules.field_remote_video`
+    `/node/news/${id}?include=field_modules,field_modules.field_image,field_modules.field_image.field_media_image,field_global_taxonomy,field_modules.field_remote_video,field_image_some,field_image_some.field_media_image`
   )
     .then(deserialize)
     .then(response => response.data as CmsArticle | null);
